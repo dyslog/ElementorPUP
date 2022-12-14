4ddb261c6713481de2d9f06b79417348c2d9f45e
dyslog <gabrieljoffe@gmail.com>
2022-12-14 14:31:26 +0100

    i am commiting

--- elem-pup-gj/elem-pup-gj.php
+++ elem-pup-gj/elem-pup-gj.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * Plugin Name: Elementor PUP
- * Description: Simple hello world widgets for Elementor.
+ * Description: GJ Elementor widgets PowerUps.
  * Version:     1.0.0
  * Author:      Gabriel Joffe
  * Author URI:  https://www.gabrieljoffe.com/
  * Text Domain: elementor-pup
  */
 
-function register_hello_world_widget( $widgets_manager ) {
+function register_ele_pup( $widgets_manager ) {
 
-	require_once( __DIR__ . '/widgets/hello-world-widget-1.php' );
+	require_once( __DIR__ . '/widgets/ele_pup_repeater.php' );
 	//require_once( __DIR__ . '/widgets/hello-world-widget-2.php' );
 
-	$widgets_manager->register( new \Elementor_Hello_World_Widget_1() );
+	$widgets_manager->register( new \Ele_Pup_Repeater() );
 	//$widgets_manager->register( new \Elementor_Hello_World_Widget_2() );
 
 }
-add_action( 'elementor/widgets/register', 'register_hello_world_widget' );
+add_action( 'elementor/widgets/register', 'register_ele_pup' );
 ?>
