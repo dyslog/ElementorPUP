5b5241e15f0f7a052985eeee691160044cfe0423
dyslog <gabrieljoffe@gmail.com>
2022-12-15 11:28:15 +0100

    rep

--- elem-pup-gj/widgets/ele_pup_repeater.php
+++ elem-pup-gj/widgets/ele_pup_repeater.php
@@ -23,38 +23,7 @@ class Ele_Pup_Repeater extends \Elementor\Widget_Base {
 
 
 
-protected function _register_controls() {
 
-	$this->start_controls_section(
-		'content_section',
-		[
-			'label' => __( 'Options', self::$slug ),
-			'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-		]
-	);
-
-	$this->add_control(
-		'widget_value',
-		[
-			'label' => __( 'value', self::$slug ),
-			'type' => \Elementor\Controls_Manager::TEXT,
-			'default' => __( 'value', self::$slug ),
-			'placeholder' => __( 'Value Attribute', self::$slug ),
-		]
-	);
-
-	$this->add_control(
-		'widget_contents',
-		[
-			'label' => __( 'contents', self::$slug ),
-			'type' => \Elementor\Controls_Manager::TEXT,
-			'default' => __( 'contents', self::$slug ),
-			'placeholder' => __( 'Option Contents', self::$slug ),
-		]
-	);
-
-	$this->end_controls_section();
-}
 
 
 
