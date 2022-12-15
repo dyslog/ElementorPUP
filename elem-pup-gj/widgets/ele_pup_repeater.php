3c02a0fe62409cb06bf88750a4f5a28e69f0eafd
dyslog <gabrieljoffe@gmail.com>
2022-12-15 18:44:46 +0100

    a

--- elem-pup-gj/widgets/ele_pup_repeater.php
+++ elem-pup-gj/widgets/ele_pup_repeater.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-class Ele_Pup_Repeater extends \Elementor\Core\DynamicTags\Tag {
+class Ele_Pup_Repeater extends \Elementor\Widget_Base {
 
 	public function get_name() {
 		return 'hello_world_widget_1';
